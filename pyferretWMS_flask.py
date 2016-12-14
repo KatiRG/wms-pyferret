bae04372bf7edbee4bc9573be52cab2593373673
katiRG <cnangini@gmail.com>
2016-12-14 14:46:52 +0100

    Test: dialog box to save example text file

--- pyferretWMS_flask.py
+++ pyferretWMS_flask.py
@@ -55,16 +55,13 @@ def map_formhandler():
     postvar = str(request.form['postvar'])
 
     session["cart"].append({'command': command, 'variable': variable, 'dataset': dataset, 'postvar': postvar})
-    print("append to cart: ",  session['cart'])
     cmdArray = session['cart']
     print("cmdArray: ", cmdArray)
     
     nbMaps = len(cmdArray)
     listSynchroMapsToSet = list(itertools.permutations(range(1,nbMaps+1), 2))
 
-    # pyferret.run('use ' + dataset)
-
-    return render_template('showmaps.html', postvar=postvar, cmdArray=cmdArray, listSynchroMapsToSet=listSynchroMapsToSet)
+    return render_template('showmaps.html', cmdArray=cmdArray, listSynchroMapsToSet=listSynchroMapsToSet)
 
 
 @app.route('/showmaps_resource', methods=['POST','GET'])
@@ -82,7 +79,6 @@ def api_calcmaps():
     COMMAND = str(request.args.get('COMMAND'))
     VARIABLE = str(request.args.get('VARIABLE'))
     print("############### DSET: ", DSET)
-    # print("############### POSTVAR: ", POSTVAR)
 
     tmpname = tempfile.NamedTemporaryFile(suffix='.png').name
     tmpname = os.path.basename(tmpname)
@@ -91,9 +87,7 @@ def api_calcmaps():
     # pyferret.run('use levitus_climatology')
     pyferret.run('use ' + DSET)
 
-    if request.args.get('REQUEST') == 'GetColorBar':                
-                # print("GetColorBar COMMAND: ", COMMAND)
-                # print("GetColorBar VARIABLE: ", VARIABLE)
+    if request.args.get('REQUEST') == 'GetColorBar':
 
                 pyferret.run('set window/aspect=1/outline=0')
                 # pyferret.run('set window/aspect=.7')
@@ -109,11 +103,8 @@ def api_calcmaps():
 
 
     elif request.args.get('REQUEST') == 'GetMap':
-        print("GetMap COMMAND: ", COMMAND)
-        print("GetMap VARIABLE: ", VARIABLE)
         
-        #Define pyferret variables for 'REQUEST' == 'GetMap'                
-        # BBOX = environ['BBOX']
+        #Define pyferret variables for 'REQUEST' == 'GetMap'
         BBOX = request.args.get('BBOX')
         BBOX = BBOX.split(',')
     
@@ -132,14 +123,19 @@ def api_calcmaps():
 
         pyferret.run('set window/aspect=1/outline=5')
         pyferret.run('go margins 0 0 0 0')    
-        # 
+        
+        #For saving to file
+        # pyferret.run('set mode meta')
 
         if POSTVAR:
             pyferret.run(COMMAND +  '/noaxis/nolab/nokey' + HLIM + VLIM + ' ' + VARIABLE + ',' + POSTVAR)
         else:
             pyferret.run(COMMAND +  '/noaxis/nolab/nokey' + HLIM + VLIM + ' ' + VARIABLE)
         
-        pyferret.run('frame/format=PNG/transparent/xpixels=' + str(WIDTH) + '/file="' + tmpdir + '/' + tmpname + '"')                
+        #For saving to file
+        pyferret.run('frame/format=PNG/transparent/xpixels=' + str(WIDTH) + '/file="' + tmpdir + '/' + tmpname + '"')
+        pyferret.run('cancel mode meta')
+        print("*************** file: ", tmpdir + '/' + tmpname)
 
     if os.path.isfile(tmpdir + '/' + tmpname):
         ftmp = open(tmpdir + '/' + tmpname, 'rb')
@@ -150,7 +146,25 @@ def api_calcmaps():
     resp = Response(iter(img), status=200, mimetype='image/png')
     return resp
 
+# Download map to file
 
+# http://code.runnable.com/UiIdhKohv5JQAAB6/how-to-download-a-file-generated-on-the-fly-in-flask-for-python
+@app.route('/download')
+def download():
+    csv = """"REVIEW_DATE","AUTHOR","ISBN","DISCOUNTED_PRICE"
+            "1985/01/21","Douglas Adams",0345391802,5.95
+            "1990/01/12","Douglas Hofstadter",0465026567,9.95
+            "1998/07/15","Timothy ""The Parser"" Campbell",0968411304,18.99
+            "1999/12/03","Richard Friedman",0060630353,5.95
+            "2004/10/04","Randel Helms",0879755725,4.50"""
+
+    # We need to modify the response, so the first thing we 
+    # need to do is create a response out of the CSV string
+    response = make_response(csv)
+    # This is the key: Set the right header for the response
+    # to be downloaded, instead of just printed on the browser
+    response.headers["Content-Disposition"] = "attachment; filename=books.csv"
+    return response
 
     
 
